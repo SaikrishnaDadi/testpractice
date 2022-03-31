3af79c4fe1dbf47378bbf64f949e10f576f8b538
SaiKrishna <saikrishnadadi81@gmail.com>
2022-03-31 23:09:50 +0530

    charwise count basic program commit

---
+++ src/com/practice/logics/StringCharWiseCount.java
@@ -0,0 +1,17 @@
+package com.practice.logics;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class StringCharWiseCount {
+
+	public static void main(String[] args) {
+		
+	}
+	
+	public static Map<Character,Integer> getCharWiseCount(String inputStr) {
+		Map<Character,Integer> charWiseCountMap = new HashMap<>();
+		return charWiseCountMap;
+	}
+	
+}
